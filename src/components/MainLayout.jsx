c0954677b83b70ceea494d33271ab2c08b50bf61
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-30 20:17:09 +0300

    added jsx component file

---
+++ src/components/MainLayout.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+
+import Header from "./Header";
+import Footer from "./Footer";
+export default function MainLayout() {
+    return (
+        <section className="main--layout">
+            <Header/>
+            <Outlet/>
+            <Footer/>
+        </section>
+    )
+}
