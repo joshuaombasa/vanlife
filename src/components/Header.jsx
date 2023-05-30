cb4288e210e27a21cc97d9ce18403553f19425c6
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-30 20:16:33 +0300

    added jsx component file

---
+++ src/components/Header.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+export default function Header() {
+
+    const activeStyles = {
+        color: "red",
+        fontWeight: "bold",
+        textDecoration: "underline"
+    }
+
+    return (
+        <header className="header--container">
+            <h1 className="logo"><NavLink to="/">#VANLIFE</NavLink></h1>
+            <nav>
+                <ul>
+                    <li>
+                        <NavLink 
+                            to="/host"
+                            style={({isActive}) => isActive ? activeStyles : null}
+                        >Host</NavLink></li>
+                    <li>
+                        <NavLink 
+                            to="/about"
+                            style={({isActive}) => isActive ? activeStyles : null}
+
+                        >About</NavLink></li>
+                    <li>
+                        <NavLink 
+                            to="/vans"
+                            style={({isActive}) => isActive ? activeStyles : null}
+                        >Vans</NavLink></li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
