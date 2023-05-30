2df3c0fc80dd6f6ce561bae8eb1ef62a76031ca3
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-30 20:17:25 +0300

    added jsx component file

---
+++ src/components/Van.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Link } from "react-router-dom";
+export default function Van({van}) {
+
+let styles 
+if (van.type === "simple") {
+   styles = {backgroundColor:"#E17654" }
+} else if (van.type === "rugged") {
+    styles = {backgroundColor: "#115E59"}  
+} else if(van.type === "luxury") {
+    styles = {backgroundColor: "#161616"}
+}
+
+    return (
+        <div className="van--container">
+            <Link to={`${van.id}`}>
+            <img src={van.imageUrl} alt="" />
+            <div className="van--info">
+                <h2>{van.name}</h2>
+                <h2>$ {van.price}<br/><span className="per--day--text--styling">/day</span></h2>
+            </div>
+            <p style={styles}>{van.type}</p>
+            </Link>
+        </div>
+    )
+}
