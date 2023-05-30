9399c34bf7a57c7fe037b7d0f248fe124211ee3a
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-30 20:20:35 +0300

    added jsx hostVans  page file

---
+++ src/pages/host/HostVans.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function HostVans() {
+    return (
+        <div className="host--vans--container">
+            <h1>This is hostimages</h1>
+        </div>
+    )
+} 
