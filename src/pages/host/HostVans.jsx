bcbbd63f0866fa2e7bccd4fc12d42f6aadd5c93d
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-05-31 23:38:15 +0300

    updated jsx

--- src/pages/host/HostVans.jsx
+++ src/pages/host/HostVans.jsx
@@ -1,9 +1,32 @@
 import React from "react";
+import HostVan from "../../components/HostVan";
 
 export default function HostVans() {
+
+    const [hostVansData, setHostVansData] =  React.useState(null)
+
+    React.useEffect(() => {
+        fetch("/api/host/vans")
+          .then(res => res.json())
+          .then(data => setHostVansData(data.vans))
+    },[])
+
+ 
+
+    let hostVansList
+
+    if (hostVansData) {
+        hostVansList = hostVansData.map(item => {
+            return <HostVan key={item.id} van={item}/>
+        })
+    }
+
     return (
+        <>
+        <h1 className="listen--vans--title">Your listed vans</h1>
         <div className="host--vans--container">
-            <h1>This is hostimages</h1>
+           {hostVansList}
         </div>
+        </>
     )
 } 
