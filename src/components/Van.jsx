7495642de0aca92f2572ac41144c78a5eb9f2d07
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-01 23:57:57 +0300

    added filter functionality

--- src/components/Van.jsx
+++ src/components/Van.jsx
@@ -1,25 +1,32 @@
 import React from "react";
-import { Link } from "react-router-dom";
-export default function Van({van}) {
+import { Link, useSearchParams } from "react-router-dom";
 
-let styles 
-if (van.type === "simple") {
-   styles = {backgroundColor:"#E17654" }
-} else if (van.type === "rugged") {
-    styles = {backgroundColor: "#115E59"}  
-} else if(van.type === "luxury") {
-    styles = {backgroundColor: "#161616"}
-}
+export default function Van({ van }) {
+
+    const [searchParams, setSearchParams] = useSearchParams()
+
+
+    let styles
+    if (van.type === "simple") {
+        styles = { backgroundColor: "#E17654" }
+    } else if (van.type === "rugged") {
+        styles = { backgroundColor: "#115E59" }
+    } else if (van.type === "luxury") {
+        styles = { backgroundColor: "#161616" }
+    }
 
     return (
         <div className="van--container">
-            <Link to={`${van.id}`}>
-            <img src={van.imageUrl} alt="" />
-            <div className="van--info">
-                <h2>{van.name}</h2>
-                <h2>$ {van.price}<br/><span className="per--day--text--styling">/day</span></h2>
-            </div>
-            <p style={styles}>{van.type}</p>
+            <Link
+                to={`${van.id}`}
+                state={{ search: `?${searchParams.toString()}`}}
+            >
+                <img src={van.imageUrl} alt="" />
+                <div className="van--info">
+                    <h2>{van.name}</h2>
+                    <h2>$ {van.price}<br /><span className="per--day--text--styling">/day</span></h2>
+                </div>
+                <p style={styles}>{van.type}</p>
             </Link>
         </div>
     )
