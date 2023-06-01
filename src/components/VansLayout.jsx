afe979e35b4bca37129c2ff69ae3cbdbc14b7bc3
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-01 23:58:18 +0300

    added filter functionality

--- src/components/VansLayout.jsx
+++ src/components/VansLayout.jsx
@@ -1,7 +1,33 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link,useSearchParams } from "react-router-dom";
 
 export default function VansLayout() {
+
+    const [searchParams, setSearchParams] = useSearchParams()
+
+    // function getNewSearchParamsString(key, value) {
+    //     const sp = new URLSearchParams(searchParams)
+    //     if (value === null) {
+    //         sp.delete(key)
+    //     } else {
+    //         sp.set(key, value)
+    //     }
+
+    //     return `?${sp.toString()}`
+    // }
+
+    function handleFilterChange(key, value) {
+        setSearchParams(prevParams => {
+            if (value === null) {
+                prevParams.delete(key)
+            } else {
+                prevParams.set(key, value)
+            }
+
+            return prevParams
+        })
+    }
+
     return (
         <div className="vans--layout--container">
             
@@ -10,24 +36,24 @@ export default function VansLayout() {
                 <ul>
                     <li 
                        className="vans--filter--buttons">
-                        <Link 
-                           to="?type=simple"
-                           >Simple</Link></li>
+                        <button 
+                           onClick={() => handleFilterChange("type", "simple")}
+                           >Simple</button></li>
                     <li 
                        className="vans--filter--buttons">
-                        <Link 
-                           to="?type=luxury"
-                           >Luxury</Link></li>
+                        <button 
+                           onClick={() => handleFilterChange("type", "luxury")}
+                           >Luxury</button></li>
                     <li 
                        className="vans--filter--buttons">
-                        <Link 
-                           to="?type=rugged"
-                           >Rugged</Link></li>
+                        <button 
+                           onClick={() => handleFilterChange("type", "rugged")}
+                           >Rugged</button></li>
                     <li 
                        className="clear--filters--link">
-                        <Link 
-                           to=""
-                           >Clear filters</Link></li>
+                        <button 
+                           onClick={() => handleFilterChange("type", null)}
+                        >Clear filters</button></li>
                 </ul>
             </nav>
         </div>
