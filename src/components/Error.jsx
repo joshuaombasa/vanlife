37d1df7f8c51a99ffd5e070e80bf45194e3551a8
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-05 17:43:37 +0300

    added error component

---
+++ src/components/Error.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { useRouteError } from "react-router-dom";
+
+export default function Error() {
+
+    const error = useRouteError()
+
+    return (
+        <div className="error--container">
+        <h1>{error.message}</h1>
+        <pre>{error.status} - {error.statusText}</pre>
+        </div>
+    )
+}
