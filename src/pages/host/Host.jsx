48297c334fbec5eacd0f88a4b7a1577986b676bd
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-06 23:36:13 +0300

    added to host route

--- src/pages/host/Host.jsx
+++ src/pages/host/Host.jsx
@@ -1,6 +1,15 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, redirect } from "react-router-dom";
 import HostLayout from "../../components/HostLayout";
+import { requireAuth } from "../../utils";
+
+export function loader() {
+    const isLoggedIn = false
+
+    if (!isLoggedIn) {
+        return redirect("/login")
+    }
+}
 
 export default function Host() {
 
