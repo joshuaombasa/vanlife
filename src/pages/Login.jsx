cb2240d3f554cda1c1af0ceb2386274a8dd3b701
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-07 21:41:16 +0300

    added action function to handle login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,33 +1,54 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate, Form, redirect } from "react-router-dom";
+import { loginUser } from "../api";
 
 export function loader({ request }) {
     return new URL(request.url).searchParams.get("message")
+}
+
+export async function action({request}) {
+    const formData = await request.formData()
+    const email = formData.get("email")
+    const password = formData.get("password")
+    const data = await loginUser({email, password})
+    localStorage.setItem("loggedin", true)
+    
+    console.log(data)
+    location.replace('/host');
+    // redirect("/host")
+    return  null
 }
 
 export default function Login() {
 
     const message = useLoaderData()
 
-    console.log(message)
-
     const [formData, setFormData] = React.useState({
         email: "",
         password: ""
     })
 
-    function handleChange(event) {
-        const {name, value} = event.target
-        setFormData(prevFormData => (
-            {
-                ...prevFormData,
-                [name] : value
-            }
-        ))
-    }
+    const [status, setStatus] = React.useState("idle")
+
+    const [error, setError] = React.useState(null)
+
+    
+
+    const navigate = useNavigate()
 
     function handleSubmit(event) {
         event.preventDefault()
+        setStatus("submitting")
+        loginUser(formData).
+            then(data => {
+                navigate("/host")
+            }).
+            catch(error => {
+                setError(error)
+                console.log("There was an error")
+            }) 
+            .finally(setStatus("idle"))
+        setError(null)
     }
 
  
@@ -36,27 +57,28 @@ export default function Login() {
         <div className="login--container">
             <h1 className="login--title">Sign in to your account</h1>
             {message && <pre className="login--error--message">{message}</pre>}
-            <form onSubmit={handleSubmit} className="login--form">
+            {error && <pre className="login--error--message">{error.message}</pre>}
+            <Form method="post" className="login--form">
                 <div className="signup--inputs--container">
                     <input 
                          className="Email--input--login" 
-                         type="text" 
+                         type="email" 
                          placeholder="Email adress"
-                         name="email"
-                         value={formData.firstName} 
-                         onChange={handleChange}
+                         name="email"   
                     />
                     <input 
                          className="Password--input--login" 
                          type="text" 
                          placeholder="Password" 
                          name="password"
-                         value={formData.lastName}
-                         onChange={handleChange}
                     />
                 </div>
-                <button>Sign in</button>
-            </form>
+                <button
+                  disabled={status === "submitting" ? true : false}
+                >
+                   {status === "submitting" ? "Logging in ..." : "Log in"}
+                </button>
+            </Form>
         </div>
     )
 }
