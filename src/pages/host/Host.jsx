19a8a04e5db4ac137521e1786d4451aeb90bf2af
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-07 21:42:50 +0300

    removed outdated loader function

--- src/pages/host/Host.jsx
+++ src/pages/host/Host.jsx
@@ -1,15 +1,8 @@
 import React from "react";
 import { Outlet, redirect } from "react-router-dom";
 import HostLayout from "../../components/HostLayout";
-import { requireAuth } from "../../utils";
 
-export function loader() {
-    const isLoggedIn = false
 
-    if (!isLoggedIn) {
-        return redirect("/login")
-    }
-}
 
 export default function Host() {
 
