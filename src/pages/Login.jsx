6c48ed48d7073c501992317a34d30f178777b626
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-08 18:29:11 +0300

    added logic to use history state for navigation in protected routes

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,56 +1,43 @@
 import React from "react";
-import { useLoaderData, useNavigate, Form, redirect } from "react-router-dom";
+import { 
+          useLoaderData, 
+          useNavigate, 
+          Form, 
+          redirect, 
+          useActionData, 
+          useNavigation 
+        } from "react-router-dom";
+
 import { loginUser } from "../api";
 
 export function loader({ request }) {
     return new URL(request.url).searchParams.get("message")
 }
 
-export async function action({request}) {
-    const formData = await request.formData()
-    const email = formData.get("email")
-    const password = formData.get("password")
-    const data = await loginUser({email, password})
-    localStorage.setItem("loggedin", true)
-    
-    console.log(data)
-    location.replace('/host');
-    // redirect("/host")
-    return  null
+export async function action({ request }) {
+    try {
+        const formData = await request.formData()
+        const email = formData.get("email")
+        const password = formData.get("password")
+        const data = await loginUser({ email, password })
+        localStorage.setItem("loggedin", true)
+        const userPrevPathname = new URL(request.url).searchParams.get("prevPath") || "/host"
+        console.log(userPrevPathname)
+        location.replace(userPrevPathname);
+        return null
+    } catch(error) {
+        return error
+    }
 }
 
 export default function Login() {
 
+    const navigation = useNavigation()
+
+    const error = useActionData()
+
     const message = useLoaderData()
 
-    const [formData, setFormData] = React.useState({
-        email: "",
-        password: ""
-    })
-
-    const [status, setStatus] = React.useState("idle")
-
-    const [error, setError] = React.useState(null)
-
-    
-
-    const navigate = useNavigate()
-
-    function handleSubmit(event) {
-        event.preventDefault()
-        setStatus("submitting")
-        loginUser(formData).
-            then(data => {
-                navigate("/host")
-            }).
-            catch(error => {
-                setError(error)
-                console.log("There was an error")
-            }) 
-            .finally(setStatus("idle"))
-        setError(null)
-    }
-    
     return (
         <div className="login--container">
             <h1 className="login--title">Sign in to your account</h1>
@@ -58,23 +45,23 @@ export default function Login() {
             {error && <pre className="login--error--message">{error.message}</pre>}
             <Form method="post" className="login--form">
                 <div className="signup--inputs--container">
-                    <input 
-                         className="Email--input--login" 
-                         type="email" 
-                         placeholder="Email adress"
-                         name="email"   
+                    <input
+                        className="Email--input--login"
+                        type="email"
+                        placeholder="Email adress"
+                        name="email"
                     />
-                    <input 
-                         className="Password--input--login" 
-                         type="text" 
-                         placeholder="Password" 
-                         name="password"
+                    <input
+                        className="Password--input--login"
+                        type="text"
+                        placeholder="Password"
+                        name="password"
                     />
                 </div>
                 <button
-                  disabled={status === "submitting" ? true : false}
+                    disabled={navigation.state === "submitting" ? true : false}
                 >
-                   {status === "submitting" ? "Logging in ..." : "Log in"}
+                    {navigation.state === "submitting" ? "Logging in ..." : "Log in"}
                 </button>
             </Form>
         </div>
