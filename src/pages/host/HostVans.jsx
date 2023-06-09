f65ef828c396c4d6162b57c90787c3a192f764f0
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-09 22:08:47 +0300

    added react suspence

--- src/pages/host/HostVans.jsx
+++ src/pages/host/HostVans.jsx
@@ -1,31 +1,39 @@
 import React from "react";
-import { useLoaderData, useParams } from "react-router-dom";
+import { useLoaderData, useParams, defer, Await } from "react-router-dom";
 import HostVan from "../../components/HostVan";
-import { getHostVans} from "../../api";
+import { getHostVans } from "../../api";
 import { requireAuth } from "../../utils";
 
-export async function loader ({request}) {
+export async function loader({ request }) {
     await requireAuth(request)
-    return getHostVans()
+    return defer({ vans: getHostVans() })
 }
 
 export default function HostVans() {
 
     const data = useLoaderData()
-    
-    const hostVansData = data.vans
-
-    const hostVansList = hostVansData.map(item => {
-            return <HostVan key={item.id} van={item}/>
-        })
-    
 
     return (
         <>
-        <h1 className="listen--vans--title">Your listed vans</h1>
-        <div className="host--vans--container">
-           {hostVansList}
-        </div>
+            <h1 className="listen--vans--title">Your listed vans</h1>
+            <React.Suspense fallback={<h1>Loading...</h1>}>
+            <Await resolve={data.vans}>
+                {(hostVans) => {
+                    
+                    const hostVansData = hostVans.vans
+                    const hostVansList = hostVansData.map(item => {
+                        return <HostVan key={item.id} van={item} />
+                    })
+
+                    return (
+                        <div className="host--vans--container">
+                            {hostVansList}
+                        </div>
+                    )
+                }}
+
+            </Await>
+            </React.Suspense>
         </>
     )
 } 
