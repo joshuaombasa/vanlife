5971fea325b151237f92c975d8372847bfa7ce89
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-06-27 23:11:51 +0300

    updated jsx logic

--- src/components/HostVanIdDetails.jsx
+++ src/components/HostVanIdDetails.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import { Link, useOutletContext } from "react-router-dom";
 export default function HostVanIdDetails() {
 
-    const {selectedVan} = useOutletContext()
+    const { selectedVan } = useOutletContext()
 
     console.log(selectedVan)
 
     return (
         <div className="host--van--id--info">
-        <h3 className="host--van--id--info">Name: <span>{selectedVan.name}</span></h3>
-        <h3 className="host--van--id--info">Category: <span>{selectedVan.type}</span></h3>
-        <h3 className="host--van--id--info">Description: <span>{selectedVan.description}</span></h3>
-        <h3 className="host--van--id--info">Visibility: <span>Public</span></h3>
+            <h3 className="host--van--id--info">Name: <span>{selectedVan.name}</span></h3>
+            <h3 className="host--van--id--info">Category: <span>{selectedVan.type}</span></h3>
+            <h3 className="host--van--id--info">Description: <span>{selectedVan.description}</span></h3>
+            <h3 className="host--van--id--info">Visibility: <span>Public</span></h3>
         </div>
     )
 }
