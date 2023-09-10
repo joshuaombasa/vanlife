5f14330215883dae6b088fbb2e73cff4848e9e26
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-10 23:54:57 +0300

    updated fetch logic

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,11 @@
 import { redirect } from "react-router-dom"
 
-export async function requireAuth() {
-    const isLoggedIn = false
-    
+export async function requireAuth(request) {
+    const isLoggedIn = localStorage.getItem("loggedin")
+    const pathname = new URL(request.url).pathname
     if (!isLoggedIn) {
-        throw redirect("/login?message=Please login first.")
-    }
+        throw redirect(`/login?message=You must log in first.&prevPath=${pathname}`)
+    } 
+
+    return null
 }
